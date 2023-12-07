fa5bb95d2940c17f3dc20882dcbf3e464f8877b1
MarcoPeruzzocmd <marcoantperuzzo@gmail.com>
2023-12-07 09:26:21 -0300

    feito

---
+++ script.js
@@ -0,0 +1,23 @@
+const tarefaInput = document.getElementById("Add-Task");
+const adicionarBotão = document.getElementById("adicionar");
+const listaTarefas = document.getElementById("tarefas");
+adicionarBotão.addEventListener("click", adicionarTarefa);
+tarefaInput.addEventListener("keypress", function (e) {
+    if(e.key === "Enter") {
+        adicionarTarefa()
+    }
+})
+function adicionarTarefa() {
+    const tarefaTexto = tarefaInput.value;
+    if (tarefaTexto.trim() !==""){
+        const novaTarefa = document.createElement("li");
+        novaTarefa.innerHTML = `${tarefaTexto}` +` <button id="botãoExcluir" class="excluir">Exlcuir</button>`
+        listaTarefas.appendChild(novaTarefa);
+        tarefaInput.value = "";
+    }
+}
+listaTarefas.addEventListener("click", function (e){
+    if(e.target.classList.contains("excluir")) {
+        e.target.parentElement.remove()
+    }
+})

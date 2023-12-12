4c677db201e8b8d0caf7e96f9f79aa1c2e1b8a24
MarcoPeruzzocmd <marcoantperuzzo@gmail.com>
2023-12-12 15:33:39 -0300

    finalizado

--- script.js
+++ script.js
@@ -2,15 +2,12 @@ const tarefaInput = document.getElementById("Add-Task");
 const adicionarBotão = document.getElementById("adicionar");
 const listaTarefasPendentes = document.getElementById("tarefas-pendentes");
 const listaTarefasFinalizadas = document.getElementById("tarefas-finalizadas");
-
 adicionarBotão.addEventListener("click", adicionarTarefa);
-
 tarefaInput.addEventListener("keypress", function (e) {
     if (e.key === "Enter") {
         adicionarTarefa();
     }
 });
-
 function adicionarTarefa() {
     const tarefaTexto = tarefaInput.value;
     if (tarefaTexto.trim() !== "") {
@@ -24,32 +21,28 @@ function adicionarTarefa() {
         tarefaInput.value = "";
     }
 }
-
 function moverTarefa(tarefa, novaLista) {
     const botaoExcluir = tarefa.querySelector(".excluir");
-    botaoExcluir.remove(); // Remove o botão de excluir
+    botaoExcluir.remove(); 
 
     const botaoDesfazer = document.createElement("button");
     botaoDesfazer.textContent = "Excluir";
     botaoDesfazer.classList.add("desfazer");
 
-    // Adiciona evento ao botão Desfazer para mover a tarefa de volta
     botaoDesfazer.addEventListener("click", function () {
         moverTarefa(tarefa, listaTarefasPendentes);
-        botaoDesfazer.remove(); // Remove o botão Desfazer
+        botaoDesfazer.remove(); 
     });
-
     tarefa.appendChild(botaoDesfazer);
     novaLista.appendChild(tarefa);
 }
-
 document.addEventListener("click", function (e) {
     if (e.target.classList.contains("excluir")) {
         const tarefa = e.target.parentElement;
         moverTarefa(tarefa, listaTarefasFinalizadas);
     } else if (e.target.classList.contains("desfazer")) {
         const tarefa = e.target.parentElement;
-        e.target.remove(); // Remove o botão Desfazer
-        tarefa.remove();   // Remove a tarefa
+        e.target.remove(); 
+        tarefa.remove();   
     }
 });
